0aecc48e28711f82434ec8106d52175fc9f29e2f
Billiam <billiamthesecond@gmail.com>
2017-11-02 18:02:55 -0500

    Explore, reinforce, destroy

--- MyBot.rb
+++ MyBot.rb
@@ -37,12 +37,50 @@ while true
     nearby_entities = map.nearby_entities_by_distance(ship)
     planets_by_distance = nearby_entities.sort.map {|_dist, list| list.select {|entity| entity.is_a? Planet }}.flatten
 
+    ship_command = nil
+
+    # explore
     planets_by_distance.each do |planet|
       next if planet.owned?
 
       if ship.can_dock?(planet)
-        command_queue << ship.dock(planet)
-      else
+        ship_command = ship.dock(planet)
+        break
+      end
+
+      # If we can't dock, we move towards the closest empty point near this
+      # planet (by using closest_point_to) with constant speed. Don't worry
+      # about pathfinding for now, as the command will do it for you.
+      # We run this navigate command each turn until we arrive to get the
+      # latest move.
+      # Here we move at half our maximum speed to better control the ships
+      # In order to execute faster we also choose to ignore ship collision
+      # calculations during navigation.
+      # This will mean that you have a higher probability of crashing into
+      # ships, but it also means you will make move decisions much quicker.
+      # As your skill progresses and your moves turn more optimal you may
+      # wish to turn that option off.
+      closest = ship.closest_point_to(planet)
+      speed = Game::Constants::MAX_SPEED
+      navigate_command = ship.navigate(closest, map, speed, max_corrections: 30)
+      # If the move is possible, add it to the command_queue (if there are too
+      # many obstacles on the way or we are trapped (or we reached our
+      # destination!), navigate_command will return null; don't fret though,
+      # we can run the command again the next turn)
+      if navigate_command
+        ship_command = navigate_command
+        break
+      end
+    end
+
+    unless ship_command
+      # reinforce
+      non_full_planets = planets_by_distance.select {|planet| planet.owner == map.me && ! planet.full? }
+      non_full_planets.each do |planet|
+        if ship.can_dock?(planet)
+          ship_command = ship.dock(planet)
+          break
+        end
         # If we can't dock, we move towards the closest empty point near this
         # planet (by using closest_point_to) with constant speed. Don't worry
         # about pathfinding for now, as the command will do it for you.
@@ -57,17 +95,39 @@ while true
         # wish to turn that option off.
         closest = ship.closest_point_to(planet)
         speed = Game::Constants::MAX_SPEED
-        navigate_command = ship.navigate(closest, map, speed, ignore_ships: false)
+        navigate_command = ship.navigate(closest, map, speed, max_corrections: 30)
         # If the move is possible, add it to the command_queue (if there are too
         # many obstacles on the way or we are trapped (or we reached our
         # destination!), navigate_command will return null; don't fret though,
         # we can run the command again the next turn)
         if navigate_command
-          command_queue << navigate_command
+          ship_command = navigate_command
+          break
         end
       end
-      break
     end
+
+    unless ship_command
+      # bomb the whole fucking planet
+      enemy_planets = planets_by_distance.select {|planet| planet.owner && planet.owner != map.me }
+
+      enemy_planets.each do |planet|
+        ## and circle planet
+        speed = Game::Constants::MAX_SPEED
+        navigate_command = ship.navigate(planet, map, speed, max_corrections: 30, ignore_ships: true, ignore_my_ships: false)
+        # If the move is possible, add it to the command_queue (if there are too
+        # many obstacles on the way or we are trapped (or we reached our
+        # destination!), navigate_command will return null; don't fret though,
+        # we can run the command again the next turn)
+        if navigate_command
+          ship_command = navigate_command
+          break
+        end
+      end
+    end
+
+    command_queue << ship_command if ship_command
   end
+
   game.send_command_queue(command_queue)
 end
