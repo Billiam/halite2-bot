68ffd38a51883a2b90897e7dea6fbf328ac870f8
Billiam <billiamthesecond@gmail.com>
2017-11-03 10:38:09 -0500

    Reinforce before exploring

--- MyBot.rb
+++ MyBot.rb
@@ -43,36 +43,47 @@ while true
 
     ship_command = nil
 
-    # explore
-    planets_by_distance.each do |planet|
-      next if planet.owned?
-
+    # reinforce
+    non_full_planets = planets_by_distance.first(4).select {|planet|!planet.full? && [map.me, nil].include?(planet.owner) }
+    non_full_planets.each do |planet|
       if ship.can_dock?(planet)
         ship_command = ship.dock(planet)
         break
       end
+    end
 
-      # If we can't dock, we move towards the closest empty point near this
-      # planet (by using closest_point_to) with constant speed. Don't worry
-      # about pathfinding for now, as the command will do it for you.
-      # We run this navigate command each turn until we arrive to get the
-      # latest move.
-      # Here we move at half our maximum speed to better control the ships
-      # In order to execute faster we also choose to ignore ship collision
-      # calculations during navigation.
-      # This will mean that you have a higher probability of crashing into
-      # ships, but it also means you will make move decisions much quicker.
-      # As your skill progresses and your moves turn more optimal you may
-      # wish to turn that option off.
-      closest = ship.closest_point_to(planet)
-      navigate_command = ship.navigate(closest, map, speed, max_corrections: 30, angular_step: 3)
-      # If the move is possible, add it to the command_queue (if there are too
-      # many obstacles on the way or we are trapped (or we reached our
-      # destination!), navigate_command will return null; don't fret though,
-      # we can run the command again the next turn)
-      if navigate_command
-        ship_command = navigate_command
-        break
+    unless ship_command
+      # explore
+      planets_by_distance.each do |planet|
+        next if planet.owned?
+
+        if ship.can_dock?(planet)
+          ship_command = ship.dock(planet)
+          break
+        end
+
+        # If we can't dock, we move towards the closest empty point near this
+        # planet (by using closest_point_to) with constant speed. Don't worry
+        # about pathfinding for now, as the command will do it for you.
+        # We run this navigate command each turn until we arrive to get the
+        # latest move.
+        # Here we move at half our maximum speed to better control the ships
+        # In order to execute faster we also choose to ignore ship collision
+        # calculations during navigation.
+        # This will mean that you have a higher probability of crashing into
+        # ships, but it also means you will make move decisions much quicker.
+        # As your skill progresses and your moves turn more optimal you may
+        # wish to turn that option off.
+        closest = ship.closest_point_to(planet)
+        navigate_command = ship.navigate(closest, map, speed, max_corrections: 30, angular_step: 3)
+        # If the move is possible, add it to the command_queue (if there are too
+        # many obstacles on the way or we are trapped (or we reached our
+        # destination!), navigate_command will return null; don't fret though,
+        # we can run the command again the next turn)
+        if navigate_command
+          ship_command = navigate_command
+          break
+        end
       end
     end
 
