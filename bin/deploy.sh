393f3062cc40e8ace23766909a0ed1b7699070f9
Billiam <billiamthesecond@gmail.com>
2017-11-09 12:31:39 -0600

    Add deployandtag, increase defense radius

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+USER_ID="2950"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-bin/build.sh
-hlt_client bot -b ./bot.zip
+hlt_client bot -b bot.zip
+
+VERSION=`curl -s "http://api.halite.io/v1/api/user/$USER_ID/bot/0" | grep -Pom 1 '(?<="version_number": )\d+'`
+git tag -f "v$VERSION"
+echo "Tagged as $VERSION"
